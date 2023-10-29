a73b7ac2656b5b06050332a0a404f899708a7814
Igor Savelyev <garikmen1997@gmail.com>
2023-10-29 20:05:14 +0300

    pr16: refactoring with app.js2

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { errors } = require('celebrate');
 const routes = require('./routes');
+const { createUser, login } = require('./controllers/users');
+const {
+  validationCreateUser,
+  validationLogin,
+} = require('./middlewares/validations');
+const auth = require('./middlewares/auth');
+const handelError = require('./middlewares/handelError');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(bodyParser.json());
 
+app.post('/signin', validationLogin, login);
+app.post('/signup', validationCreateUser, createUser);
+
+app.use(auth);
 app.use(routes);
+app.use(errors());
+app.use(handelError);
 
 mongoose.connect('mongodb://127.0.0.1/mestodb', {
   useNewUrlParser: true,
