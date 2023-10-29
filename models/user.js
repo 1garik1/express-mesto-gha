a859c06bba399f831f1e1cd0f72e0b53a43cc7f1
Igor Savelyev <garikmen1997@gmail.com>
2023-10-29 21:09:48 +0300

    pr16: refactoring with app.js4

--- models/user.js
+++ models/user.js
@@ -23,7 +23,7 @@ const userSchema = new mongoose.Schema({
     required: true,
     minlength: 2,
     maxlength: 30,
-    default: 'Исследователь океана',
+    default: 'Исследователь',
   },
   avatar: {
     type: String,
